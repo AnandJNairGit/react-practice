113d50a9a3e1779a16d5f45bc050267b6e4e2b14
AnandJNairGit <anandjnairit@gmail.com>
2022-10-09 00:24:37 +0530

    rename main component

---
+++ src/components/challenge3/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Footer from "./Footer";
+import Nav from "./Nav";
+import Main from "./Main";
+
+
+
+
+export default function Challenge(){
+    return(
+        <>
+            <Nav />
+            <Main />
+            <Footer />
+        </>
+    );
+}
