7543d69041d43ab027b6f822028867d4a5175373
AnandJNairGit <anandjnairit@gmail.com>
2022-10-08 21:04:19 +0530

    format the app.js file

--- src/App.js
+++ src/App.js
@@ -1,56 +1,52 @@
-import React from 'react'
+import React from "react";
 function App() {
-
-
-  function Nav(){
-    return(
-        <nav>
-            <img src="https://picsum.photos/id/237/200/300
-" width="40px"/>
-        </nav>
+  function Nav() {
+    return (
+      <nav>
+        <img
+          src="https://picsum.photos/id/237/200/300
+"
+          width="40px"
+        />
+      </nav>
     );
-}
+  }
 
-function Title(){
-    return(
-        <h1>Why I Love React</h1>
-    );
-}
+  function Title() {
+    return <h1>Why I Love React</h1>;
+  }
 
-function List(){
-    return(      
+  function List() {
+    return (
       <ol>
-            <li>It's a popular library, so I'll be 
-            able to fit in with the cool kids!</li>
-            <li>I'm more likely to get a job as a developer
-            if I know React</li>
-        </ol>);
-        }
-        
-        
-function Header(){
-    return(
-        <header>
-            <Nav/>
-            <Title/>
-            <List />
-        </header>
+        <li>
+          It's a popular library, so I'll be able to fit in with the cool kids!
+        </li>
+        <li>I'm more likely to get a job as a developer if I know React</li>
+      </ol>
     );
-}
+  }
 
-
-function Footer(){
-    return(
-        <footer>© 2022 react development. All rights reserved. </footer>
+  function Header() {
+    return (
+      <header>
+        <Nav />
+        <Title />
+        <List />
+      </header>
     );
-}
-  
+  }
+
+  function Footer() {
+    return <footer>© 2022 react development. All rights reserved. </footer>;
+  }
+
   return (
     // challenge 2
     <>
-    <Header /> 
-    <Footer />       
-</>
+      <Header />
+      <Footer />
+    </>
   );
 }
 
