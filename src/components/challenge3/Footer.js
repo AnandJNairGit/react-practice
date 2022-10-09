79a6bb1d8849e7c8273f6705eeba913921e29187
AnandJNairGit <anandjnairit@gmail.com>
2022-10-09 15:42:19 +0530

    code refactor

--- src/components/challenge3/Footer.js
+++ src/components/challenge3/Footer.js
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Footer() {
-    return <footer>© 2022 react development. All rights reserved. </footer>;
+    return <footer><small>© 2022 react development. All rights reserved.</small> </footer>;
   }
--- src/components/challenge3/Nav.js
+++ src/components/challenge3/Nav.js
@@ -1,11 +1,13 @@
 import React from "react";
+import img from "../../assets/react-icon-small.png";
 
 export default function Nav() {
     return (
       <nav style={{"display":"flex", "justifyContent":"space-between"}}>
         <img
-          src="https://picsum.photos/id/237/200/300"
+          src= {img}
           width="40px"
+          height="40px"
         />
         <ul style={{ listStyle: "none", display: "flex", gap: "10px" }}>
           <li>Pricing</li>
