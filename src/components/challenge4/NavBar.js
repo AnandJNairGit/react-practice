febd8f138c409238fafcd1feaa0df1b8e949c6a1
AnandJNairGit <anandjnairit@gmail.com>
2022-10-09 17:42:20 +0530

    add nav bar

--- src/components/challenge4/NavBar.js
+++ src/components/challenge4/NavBar.js
@@ -4,7 +4,15 @@ import logo from "../..//assets/react-icon-small.png";
 export default function NavBar() {
     return (
         <nav>
+            <div className="nav-container">
+            <div className="logo-container">
             <img className="logo" src={logo}/>
+            <h6>ReactFacts</h6>
+            </div>
+            <h3>React-Course Project-1</h3>
+            </div>
+
+
         </nav>
     );
   }
