c968f4f9183d9841478789272df4e33e3ac9eb12
AnandJNairGit <anandjnairit@gmail.com>
2022-10-11 20:53:40 +0530

    import contact card component

--- src/components/challenge5/index.js
+++ src/components/challenge5/index.js
@@ -1,11 +1,43 @@
 import React from "react";
+import ContactCard from "./ContactCard";
 import "./style.css";
-
+import felix from "../../assets/felix.png";
+import fluffy from "../../assets/fluffykins.png";
+import whisker from "../../assets/mr-whiskerson.png";
+import pumkin from "../../assets/pumpkin.png"
+ 
 
 export default function Challenge(){
     return(
         <>
-            
+            <div className="container">
+            <ContactCard 
+                img={whisker}
+                name="Mr. Whiskerson"
+                phone="(212) 555-1234"
+                email="mr.whiskaz@catnap.meow"
+            />
+            <ContactCard 
+                img={fluffy}
+                name="Fluffykins"
+                phone="(212) 555-2345"
+                email="fluff@me.com"
+            />
+            <ContactCard 
+                img={felix}
+                name="Felix"
+                phone="(212) 555-4567"
+                email="thecat@hotmail.com"
+            />
+            <ContactCard 
+                img={pumkin}
+                name="Pumpkin"
+                phone="(0800) CAT KING"
+                email="pumpkin@scrimba.com"
+            />
+
+
+            </div>
         </>
     );
 }
