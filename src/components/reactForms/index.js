91ac3b900ba3579a6af71b254f994fcfbd814915
AnandJNairGit <anandjnairit@gmail.com>
2022-10-13 17:53:57 +0530

    create a form component

---
+++ src/components/reactForms/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./style.css";
+
+
+export default function ReactForms(){
+    return(
+        <>
+        <form>
+            <label>Name</label>
+            <input type="text" />
+
+            <label>Body</label>
+            <textarea></textarea>
+        </form>
+        </>
+    );
+}
