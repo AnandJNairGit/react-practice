c5f41b45811b644d7202ae3c2e57548ad7a8140f
AnandJNairGit <anandjnairit@gmail.com>
2022-10-16 15:46:13 +0530

    create the fetched list component

---
+++ src/components/HOC/FetchedList.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+export const FetchedList = (props) => {
+  const { fetchTerm, fetchSize, dataExtractKey } = props;
+  const [data, setData] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const getData = async () => {
+      setIsLoading(true);
+      const baseUrl = "https://random-data-api.com/api/v2/";
+      const size = `?size=${fetchSize}`;
+      const res = await fetch("${baseUrl + fetchTerm + size}}");
+      const json = await res.json();
+      console.log("the json data is ---------->", json);
+      const data = json.map((item) => {
+        return item[dataExtractKey];
+      });
+      setData(data);
+      console.log("the data is----------->", data);
+      setIsLoading(false);
+    };
+    getData();
+  }, []);
+
+  const displayList = !isLoading
+    ? data.map((item, index) => {
+        return <h5 key={index}>{item}</h5>;
+      })
+    : "";
+
+  return (
+    <div style={{ margin: "100px" }}>
+      <h1>Address</h1>
+      {displayLists}
+    </div>
+  );
+};
