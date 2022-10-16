e8d90fd1aaaf794a2ed4c222fb1e5ce4d8bd3a6c
AnandJNairGit <anandjnairit@gmail.com>
2022-10-16 16:01:01 +0530

    use fetched list component to display the users

--- src/components/HOC/Address.js
+++ src/components/HOC/Address.js
@@ -2,10 +2,13 @@ import React from "react";
 import { FetchedList } from "./FetchedList";
 
 const Address = () => {
-
   return (
-  <FetchedList fetchTerm="addresses" fetchSize={20} dataExtractKey="street_address"/>
-  );
+    <FetchedList
+      fetchTerm="addresses"
+      fetchSize={20}
+      dataExtractKey="street_address"
+    />
+  );   
 };
 
 export default Address;
--- src/components/HOC/User.js
+++ src/components/HOC/User.js
@@ -2,29 +2,30 @@ import React, { useEffect, useState } from "react";
 
 const User = () => {
 
-    const [data, setData] = useState({  });
-    const [isLoading, setIsLoading ] = useState(true);   
+  //   const [data, setData] = useState({  });
+  //   const [isLoading, setIsLoading ] = useState(true);   
 
-  useEffect(() => {
-    const getData = async () => {
-      setIsLoading(true);
-      const res = await fetch("https://random-data-api.com/api/v2/users?size=20");
-      const json = await res.json();
-      const data = json.map((item)=>{return (item.first_name);});
-      setData(data);
-      console.log("the data is----------->", data);
-      setIsLoading(false);
-    };
-    getData();
-  },[]);
+  // useEffect(() => {
+  //   const getData = async () => {
+  //     setIsLoading(true);
+  //     const res = await fetch("https://random-data-api.com/api/v2/users?size=20");
+  //     const json = await res.json();
+  //     const data = json.map((item)=>{return (item.first_name);});
+  //     setData(data);
+  //     console.log("the data is----------->", data);
+  //     setIsLoading(false);
+  //   };
+  //   getData();
+  // },[]);
 
-  const users = !isLoading ? data.map((item,index)=>{return (<h5 key={index}>{item}</h5>);}) : "";
+  // const users = !isLoading ? data.map((item,index)=>{return (<h5 key={index}>{item}</h5>);}) : "";
 
   return (
-    <div style={{"margin":"100px"}}>
-        <h1>Users</h1>
-      {users}
-    </div>
+    <FetchedList
+      fetchTerm="users"
+      fetchSize={20}
+      dataExtractKey="first_name"
+    />
   );
 };
 
