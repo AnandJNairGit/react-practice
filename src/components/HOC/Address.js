ab0446acf01c6bc9178d06a742c07975bff8aa40
AnandJNairGit <anandjnairit@gmail.com>
2022-10-16 15:54:47 +0530

    use fetched list component to display the address

--- src/components/HOC/Address.js
+++ src/components/HOC/Address.js
@@ -1,31 +1,29 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import { FetchedList } from "./FetchedList";
 
 const Address = () => {
 
-    const [data, setData] = useState({  });
-    const [isLoading, setIsLoading ] = useState(true);   
+  //   const [data, setData] = useState({  });
+  //   const [isLoading, setIsLoading ] = useState(true);   
 
-  useEffect(() => {
-    const getData = async () => {
-      setIsLoading(true);
-      const res = await fetch("https://random-data-api.com/api/v2/addresses?size=20");
-      const json = await res.json();
-      console.log("the json data is ---------->", json);
-      const data = json.map((item)=>{return (item.street_address);});
-      setData(data);
-      console.log("the data is----------->", data);
-      setIsLoading(false);
-    };
-    getData();
-  },[]);
+  // useEffect(() => {
+  //   const getData = async () => {
+  //     setIsLoading(true);
+  //     const res = await fetch("https://random-data-api.com/api/v2/addresses?size=20");
+  //     const json = await res.json();
+  //     console.log("the json data is ---------->", json);
+  //     const data = json.map((item)=>{return (item.street_address);});
+  //     setData(data);
+  //     console.log("the data is----------->", data);
+  //     setIsLoading(false);
+  //   };
+  //   getData();
+  // },[]);
 
-  const users = !isLoading ? data.map((item,index)=>{return (<h5 key={index}>{item}</h5>);}) : "";
+  // const addresses = !isLoading ? data.map((item,index)=>{return (<h5 key={index}>{item}</h5>);}) : "";
 
   return (
-    <div style={{"margin":"100px"}}>
-        <h1>Address</h1>
-      {users}
-    </div>
+  <FetchedList fetchTerm="addresses" fetchSize={20} dataExtractKey="street_address"/>
   );
 };
 
