3e9f5cec4da7a0f8718cd361c34656aad1b0c702
AnandJNairGit <anandjnairit@gmail.com>
2022-10-16 15:57:39 +0530

    remove unwanted code in the address component

--- src/components/HOC/Address.js
+++ src/components/HOC/Address.js
@@ -3,25 +3,6 @@ import { FetchedList } from "./FetchedList";
 
 const Address = () => {
 
-  //   const [data, setData] = useState({  });
-  //   const [isLoading, setIsLoading ] = useState(true);   
-
-  // useEffect(() => {
-  //   const getData = async () => {
-  //     setIsLoading(true);
-  //     const res = await fetch("https://random-data-api.com/api/v2/addresses?size=20");
-  //     const json = await res.json();
-  //     console.log("the json data is ---------->", json);
-  //     const data = json.map((item)=>{return (item.street_address);});
-  //     setData(data);
-  //     console.log("the data is----------->", data);
-  //     setIsLoading(false);
-  //   };
-  //   getData();
-  // },[]);
-
-  // const addresses = !isLoading ? data.map((item,index)=>{return (<h5 key={index}>{item}</h5>);}) : "";
-
   return (
   <FetchedList fetchTerm="addresses" fetchSize={20} dataExtractKey="street_address"/>
   );
