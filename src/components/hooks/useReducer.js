d3208b0ba8c5fce8e2ca4c99c648c74b2075179f
AnandJNairGit <anandjnairit@gmail.com>
2022-10-17 22:30:52 +0530

    use reduser hook for setting the state

--- src/components/hooks/useReducer.js
+++ src/components/hooks/useReducer.js
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useReducer } from "react";
 
 const UseReducer = () => {
+  const reducer = (state, action) => {
+    console.log("inside reducer");
+    switch (action.type) {
+      case "increment":
+        return { count: state.count + 1, showText: !state.showText };
+        break;
+
+      default:
+        return state;
+        break;
+    }
+  };
+
+  const [state, dispatch] = useReducer(reducer, { count: 0, showText: true });
   return (
-    <>
+    <div style={{ margin: "50px" }}>
       <h1>UseReducer</h1>
-      <h2>1</h2>
-      <button>Test UseReducer</button>
-      <h5>Toggle this text</h5>
-    </>
+      <h2>{state.count}</h2>
+      <button
+        onClick={() => {
+          dispatch({ type: "increment" });
+        }}
+      >
+        Test UseReducer
+      </button>
+      {state.showText ? <h5>Toggle this text</h5> : ""}
+    </div>
   );
 };
 
