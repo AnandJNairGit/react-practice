37b83e6e24e08f752a16862b546c71bbf79f7424
AnandJNairGit <anandjnairit@gmail.com>
2022-10-17 20:47:00 +0530

    add dummy text and button

--- src/components/hooks/useReducer.js
+++ src/components/hooks/useReducer.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const UseReducer = () => {
   return (
-    <div>useReducer</div>
-  )
-}
+    <>
+      <h1>UseReducer</h1>
+      <h2>1</h2>
+      <button>Test UseReducer</button>
+      <h5>Toggle this text</h5>
+    </>
+  );
+};
 
-export default UseReducer
+export default UseReducer;
