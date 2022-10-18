16917ecf33ef90eea47116da274736f092228b36
AnandJNairGit <anandjnairit@gmail.com>
2022-10-19 02:25:45 +0530

    create new context data

--- src/components/hooks/Context/index.js
+++ src/components/hooks/Context/index.js
@@ -2,14 +2,19 @@ import React, { createContext } from "react";
 import CompA from "./CompA";
 
 const ContextData = createContext();
-const Contexts = () => {
+const ContextData1 = createContext();
 
+const Contexts = () => {
   return (
     <ContextData.Provider value={"this is Context Data from provider"}>
-      <CompA />
+      <ContextData1.Provider
+        value={"this is context data from second provider"}
+      >
+        <CompA />
+      </ContextData1.Provider>
     </ContextData.Provider>
   );
 };
 
 export default Contexts;
-export {ContextData}; 
+export { ContextData };
