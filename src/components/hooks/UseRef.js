7e088b31a747375031998ca4911787eaa4899bfa
AnandJNairGit <anandjnairit@gmail.com>
2022-10-18 16:12:04 +0530

    create useRef component to practice useRef hook

---
+++ src/components/hooks/UseRef.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const UseRef = () => {
+  return (
+    <div>
+        <h1>UseRef</h1>
+        <h4>Count is : 1</h4>
+        <button>Count Button</button>
+        <hr />
+        <input type="text"/>
+        <button>Focus Input</button>
+    </div>
+  )
+}
+
+export default UseRef
