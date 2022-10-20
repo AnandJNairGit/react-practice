c4d8243e91c296453ada97e6c31d5cd38a1f9483
AnandJNairGit <anandjnairit@gmail.com>
2022-10-20 15:02:48 +0530

    remove one context provider and consumer

--- src/components/hooks/Context/index.js
+++ src/components/hooks/Context/index.js
@@ -2,19 +2,14 @@ import React, { createContext } from "react";
 import CompA from "./CompA";
 
 const ContextData = createContext();
-const ContextData1 = createContext();
 
 const Contexts = () => {
   return (
     <ContextData.Provider value={"this is Context Data from provider"}>
-      <ContextData1.Provider
-        value={"this is context data from second provider"}
-      >
-        <CompA />
-      </ContextData1.Provider>
+      <CompA />
     </ContextData.Provider>
   );
 };
 
 export default Contexts;
-export { ContextData, ContextData1 };
+export { ContextData};
