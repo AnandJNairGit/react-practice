99e9c255e72aac8309c7e7eb4533b5b49c13c852
AnandJNairGit <anandjnairit@gmail.com>
2022-10-20 17:49:39 +0530

    initialize state data and pass to the provider

--- src/components/hooks/Context/index.js
+++ src/components/hooks/Context/index.js
@@ -1,15 +1,18 @@
 import React, { createContext } from "react";
 import CompA from "./CompA";
+import { useState } from "react";
 
 const ContextData = createContext();
 
 const Contexts = () => {
+  const [data, setData] = useState("initial Data");
   return (
-    <ContextData.Provider value={"this is Context Data from provider"}>
+    <ContextData.Provider value={{ data: data, setData: setData }}>
+      <h1>CONTEXT API</h1>
       <CompA />
     </ContextData.Provider>
   );
 };
 
 export default Contexts;
-export { ContextData};
+export { ContextData };
