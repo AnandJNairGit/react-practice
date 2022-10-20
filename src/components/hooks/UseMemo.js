37b5dbc836e05eb3f24271511c46b39d3648e644
AnandJNairGit <anandjnairit@gmail.com>
2022-10-21 02:28:21 +0530

    simulate a slow function

--- src/components/hooks/UseMemo.js
+++ src/components/hooks/UseMemo.js
@@ -1,13 +1,29 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const UseMemo = () => {
   const [counter, setCounter] = useState(0);
+
+  const count = getCount(counter);
   return (
     <>
-      <h2>The Count is :{counter}</h2>
-      <button onClick={()=>{setCounter(counter+1);}}>Increase count</button>
+      <h2>The Count is :{count}</h2>
+      <button
+        onClick={() => {
+          setCounter(count + 1);
+        }}
+      >
+        Increase count
+      </button>
     </>
-  )
-}
+  );
+};
+
+
+const getCount = (counter) => {
+  for (let index = 0; index <= 1000000000; index++) {
+    // console.log("h");}
+  }
+  return (counter*2)/2;
+};
 
 export default UseMemo;
