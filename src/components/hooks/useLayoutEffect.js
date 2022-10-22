d46ae3cd297b8565eea29c5eb55bc1ce8abd079e
AnandJNairGit <anandjnairit@gmail.com>
2022-10-22 23:48:18 +0530

    use useLayoutEffect Hook

--- src/components/hooks/useLayoutEffect.js
+++ src/components/hooks/useLayoutEffect.js
@@ -1,6 +1,8 @@
-import React, { useEffect, useLayoutEffect } from "react";
+import React, { useEffect, useLayoutEffect, useState } from "react";
 
 const UseLayoutEffect = () => {
+  const [count, setCount] = useState(0);
+
   useEffect(() => {
     console.log("inside use effect 1");
   });
@@ -13,9 +15,29 @@ const UseLayoutEffect = () => {
     console.log("inside use effect 2");
   });
 
+  useLayoutEffect(() => {
+    for (let index = 0; index < 1000000000; index++) {
+      if (index == 1000000000 - 1) {
+        if (count == 0) {
+          setCount(534534);
+        }
+      }
+    }
+  }, [count]);
+
+  const handleChange = () => {};
+
   return (
     <>
-      <h1>useLayoutEffect</h1>  
+      <h1>useLayoutEffect</h1>
+      <h3>{count}</h3>
+      <button
+        onChange={() => {
+          setCount(0);
+        }}
+      >
+        Click Me
+      </button>
     </>
   );
 };
